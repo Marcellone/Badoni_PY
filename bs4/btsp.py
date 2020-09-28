4f8c80bdd342f7cd70765a2fa3b496a823a87a0e
Federico Zotti <3262265-Fede-26@users.noreply.gitlab.com>
2020-09-28 18:57:26 +0200

    added webhook functionality

--- bs4/btsp.py
+++ bs4/btsp.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python
 
 import requests
+import time
 from bs4 import BeautifulSoup
+from discord_webhooks import DiscordWebhooks
+webhook=DiscordWebhooks("https://discord.com/api/webhooks/760050656542326785/rDFVNuo953TiTgWSqTeJG12NiggjiFjwmb7ukP6CjEApa73qCW6BEJLsgrOPLy65cAmV")
+
+while True:
+
+    with open("lastEntry.txt", "r") as f:
+        lastEntry = f.read()
+    isLast = False
+
+    urlBadoni = "https://www.iisbadoni.edu.it/categoria/circolari"
+    req = requests.get(urlBadoni)
+    soup = BeautifulSoup(req.text, "lxml")
+
+    itemsList = soup.find("div", {"class": "view-content"})
+    circolari = []
+
+    for item in itemsList.find_all("div", {"class": "views-row"}):
+        for head in item.find_all("a"):
+            if head:
+                if head.text != "Leggi tutto ...":
+                    titolo = head.text
+        
+                elif head.text == "Leggi tutto ...":
+                    link = "https://www.iisbadoni.edu.it/" + head["href"]
+
+        sommario = item.find("p").text
+        dataEmissione = item.find("span", {"class": "views-field-created"})
+        dataEmissione = dataEmissione.find("span", {"class": "field-content"}).text
+
+        circolari.append({"titolo": titolo,
+                          "sommario": sommario,
+                          "data": dataEmissione,
+                          "url": link})
+
+    for item in circolari:
+        if item["titolo"] == lastEntry:
+            print("non ci sono circolari")
+            isLast = True
 
 
-with open("lastEntry.txt", "r") as f:
-    lastEntry = f.read()
+        if not isLast:
+            print(item)
+            webhook.set_content(title=item["titolo"],
+                                description=item["sommario"]+"\nData: "+item["data"],
+                                url=item["url"],
+                                color=0xFF0000)
+            webhook.set_author(name="Nuova circolare")
+            webhook.set_footer(text="Badoni circolari")
+            webhook.send()   
+            print("webhook inviato")
+            print("\n\n")
+            time.sleep(1)
 
-urlBadoni = "https://www.iisbadoni.edu.it/categoria/circolari"
-req = requests.get(urlBadoni)
-soup = BeautifulSoup(req.text, "lxml")
-
-itemsList = soup.find("div", {"class": "view-content"})
-
-c = 0
-for item in itemsList.find_all("div", {"class": "views-row"}):
-    titolo = item.find("a")
-    if titolo:
-        titolo = titolo.text
-        if titolo != "Leggi tutto ...":
-            if titolo == lastEntry:
-                print("Non ci sono circolari")
-                break
-            if c == 0:
-                lastEntry = titolo
-                with open("lastEntry.txt", "w+") as f:
-                    f.write(lastEntry)
-            print(titolo)
-
-    sommario = item.find("p").text
-
-    print(sommario)
-
-    dataEmissione = item.find("span", {"class": "views-field-created"})
-    dataEmissione = dataEmissione.find("span", {"class": "field-content"})
-    print(dataEmissione.text)
-    print("\n\n")
-
-    c += 1
+    with open("lastEntry.txt", "w+") as f:
+        f.write(circolari[0]["titolo"])
+    time.sleep(240) 
