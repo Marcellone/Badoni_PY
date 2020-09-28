bea24087e2ceedbac6950fc087faafca04ff32ad
Alessandro <alessandromarcellinodj@gmail.com>
2020-09-28 16:45:25 +0200

    adapted

--- Badoni_PY.py
+++ Badoni_PY.py
@@ -14,9 +14,9 @@ import time
 chrome_options = Options()
 # chrome_options.add_argument("--headless") #Per farlo eseguire in background
 chrome_options.add_argument("disable-gpu") #Per evitare bugs
-chrome_options.add_argument("log-level=3") #Solo errori (Rimuove logs inutili)
+# chrome_options.add_argument("log-level=3") #Solo errori (Rimuove logs inutili)
 chrome_options.add_argument("window-size=1920,1080") #Imposta dimensione finestra essendo headless
-driver=webdriver.Chrome(executable_path="chromedriver",chrome_options=chrome_options) #Load per google chrome come webdriver
+driver=webdriver.Chrome(executable_path="C:/Users/aless/Desktop/Badoni_PY/dipendenza/chromedriver",options=chrome_options) #Load per google chrome come webdriver
 
 #-------------------carica il webhook di discord------------------
 
@@ -100,4 +100,3 @@ while True:
         f.write(circolari[0]["titolo"])
 
 #---------------------------------------------------------------------------------------------------------------------------------
-
