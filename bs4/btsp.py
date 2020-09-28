2cbb8e6647cc5d1c0a683f4f39aaeb09a538c9a8
Federico Zotti <3262265-Fede-26@users.noreply.gitlab.com>
2020-09-28 17:39:03 +0200

    added feature doesn't repeat last entry

--- bs4/btsp.py
+++ bs4/btsp.py
@@ -3,18 +3,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+with open("lastEntry.txt", "r") as f:
+    lastEntry = f.read()
+
 urlBadoni = "https://www.iisbadoni.edu.it/categoria/circolari"
 req = requests.get(urlBadoni)
 soup = BeautifulSoup(req.text, "lxml")
 
 itemsList = soup.find("div", {"class": "view-content"})
 
-
+c = 0
 for item in itemsList.find_all("div", {"class": "views-row"}):
     titolo = item.find("a")
     if titolo:
         titolo = titolo.text
         if titolo != "Leggi tutto ...":
+            if titolo == lastEntry:
+                print("Non ci sono circolari")
+                break
+            if c == 0:
+                lastEntry = titolo
+                with open("lastEntry.txt", "w+") as f:
+                    f.write(lastEntry)
             print(titolo)
 
     sommario = item.find("p").text
@@ -25,3 +36,5 @@ for item in itemsList.find_all("div", {"class": "views-row"}):
     dataEmissione = dataEmissione.find("span", {"class": "field-content"})
     print(dataEmissione.text)
     print("\n\n")
+
+    c += 1
